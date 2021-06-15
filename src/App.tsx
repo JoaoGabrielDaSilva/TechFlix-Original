9fc38f02d8c383ea3a2554a49b6a8b3fd5df245e
João Gabriel <joao.gabsilva1007@gmail.com>
2021-06-15 09:45:24 -0300

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   useEffect(() => {
     const signed = localStorage.getItem('signed') === 'true' ? true : false
     setAuthenticated(signed)
-  }, [])
+  }, [authenticated])
 
   return (
     <SearchProvider>
